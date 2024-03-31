b25670c4b0133a22ff9f475aa9af0df684f09f74
TecnologiaMan <165471929+TecnologiaMan@users.noreply.github.com>
2024-03-31 11:35:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -156,14 +156,13 @@ def verification(choice):
         print(f"não foi encontrado o comando: {Fore.RED}{choice}{Fore.RESET}")
 
 if __name__ == "__main__":
-    manual = ""
+    
     os.system("clear")
     print(f"{Fore.GREEN}Criado Por: TecnologiaMan\n{Fore.BLUE}Versão: {VERSION} essa versão não contem suporte pra extração .zip{Fore.RESET}")
     time.sleep(1)
     print(f"{Fore.RED}{title}{Fore.RESET}")
     print(f"The TOOL manual was created in {Fore.CYAN}{os.getcwd()}{Fore.RESET}")
     with open("manual.txt", "w") as file:
-        print(manual)
         file.write("manual: - 1-'install' this option is the default option of the tool which will simply generate the offline installer for you, follow the instructions after typing\nSorry, the instructions are in Portuguese, but they are easy to understand\n2- 'stringinstall'\nThis option allows you to install a program with binaries in a string or file if the binary is written inside it as if it were text, understand?")
 
     choice = str(input("\n\n\n[TOOL]# "))
